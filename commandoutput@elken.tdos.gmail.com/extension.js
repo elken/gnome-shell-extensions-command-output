4862eec7d176b2a7df86eda8c0f129aed7b1bdf9
Ellis Kenyo <elken.tdos@gmail.com>
2014-12-24 21:46:02 +0000

    Stuff works! Need to polish tomorrow then branch

--- commandoutput@elken.tdos.gmail.com/extension.js
+++ commandoutput@elken.tdos.gmail.com/extension.js
@@ -5,6 +5,7 @@ const Gio = imports.gi.Gio;
 const Lang = imports.lang;
 const Mainloop = imports.mainloop;
 const Main = imports.ui.main;
+const Util = imports.misc.util;
 
 const Extension = imports.misc.extensionUtils.getCurrentExtension();
 const Settings = Extension.imports.settings;
@@ -14,30 +15,24 @@ const LOCALE = GLib.get_language_names()[1];
 
 const CommandOutput = new Lang.Class({
         Name: 'CommandOutput.Extension',
-        _outputLabel: new St.Label({text: "Starting up.."}),
-        _output: new St.Bin({reactive: false, track_hover: false}),
-        _settings: Settings.getSchema(Extension),
-        _command: null,
-        _refreshRate: null,
-        _timeout: null,
-        _changeTimeout: false,
 
         enable: function() {
             this._output.set_child(this._outputLabel);
-            this._timeout = null;
-            this._changeTimeout = true;
             this._update();
-            //Mainloop.timeout_add(1000, Lang.bind(this, this._update));
+            this._output.connect('button-release-event', this._openSettings);
             Main.panel._rightBox.insert_child_at_index(this._output, 0);
         },
 
         disable: function() {
+            this._save();
             Main.panel._rightBox.remove_child(this._output);
         },
 
         _init:  function() {
-            this._command = this._settings.get_string(Prefs.Keys.COMMAND);
-            this._refreshRate = this._settings.get_int(Prefs.Keys.RATE);
+            this._settings = Settings.getSchema(Extension);
+            this._load();
+            this._outputLabel = new St.Label({text: "Starting up.."});
+            this._output = new St.Bin({reactive: false, track_hover: false});
         },
 
         _doCommand: function() {
@@ -58,16 +53,22 @@ const CommandOutput = new Lang.Class({
         _update: function() {
             let iText = this._doCommand();
             this._outputLabel.set_text(iText);
-            //this._removeTimeout();
-            this._timeout = Mainloop.timeout_add_seconds(this._refreshRate, Lang.bind(this, this._update()));
+            Mainloop.timeout_add_seconds(1, Lang.bind(this, this._update));
         },
 
-        _removeTimeout: function() {
-            if(this._timeout) {
-                Mainloop.source_remove(this._timeout);
-                this._timeout = null;
-            }
-        }
+        _load: function() {
+            this._command = this._settings.get_string(Prefs.Keys.COMMAND);
+            this._refreshRate = this._settings.get_int(Prefs.Keys.RATE);
+        },
+
+        _save: function() {
+            this._settings.set_string(Prefs.Keys.COMMAND, this._command);
+            this._settings.set_int(Prefs.Keys.RATE, this._refreshRate);
+        },
+
+        _openSettings: function () {
+            Util.spawn(["gnome-shell-extension-prefs", Extension.uuid]);
+        },
 });
 
 function init() {
